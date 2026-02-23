162fe901f130848f893e7b9602b4fe84bc47146d
Sudhanshu Chauhan <chauhansudhanshu234@gmail.com>
2026-02-23 23:09:14 +0530

    Making Front page Dynamic Using JavaScript

---
+++ js/data.js
@@ -0,0 +1,83 @@
+const restaurants = [
+  {
+    name: "Hot Bite",
+    address: "Near Tirwa Tehsil Kannauj Road",
+    image: "assests/hotbite.jpeg",
+    link: "html/hotbite.html"
+  },
+  {
+    name: "Mughal Zaika",
+    address: "Shukla colony Gandhi Nagar, tirwa",
+    image: "assests/Mughal.jpg",
+    link: "2mughal.html"
+  },
+  {
+    name: "Shukla Restaurant",
+    address: "Near Tirwa, Kannauj",
+    image: "assests/shukla.webp",
+    link: "1shukla.html"
+  }
+];
+
+const bookstores = [
+  {
+    name: "Kamal Pustak Bhandar",
+    address: "Kashap Bhawan Road, Tirwa Kannauj",
+    image: "assests/kamal.webp",
+    link: "HTML/kamal.html"
+  },
+  {
+    name: "Om Pustak Bhandar",
+    address: "Khairnagar road, Tirwaganj Kannauj",
+    image: "assests/om pustak.jpg",
+    link: "HTML/pustak.html"
+  },
+  {
+    name: "Mishra BookStore",
+    address: "Saraimeera, Kannauj",
+    image: "assests/mishra pustak.jpg",
+    link: "HTML/Mishra.html"
+  }
+];
+const restaurantvariety = 
+[
+  {
+  name: "Visit With Friend",
+  address:"Explicitly created to commemorate a person or event,serves as a memorial",
+  image:"assests/1.jpeg",
+  link:"variety.html"
+},
+  {
+  name: "Advanture",
+  address:"Explicitly created to commemorate a person or event,serves as a memorial",
+  image:"assests/2.jpeg",
+  link:"variety.html"
+},
+  {
+  name: "Shopping",
+  address:"Explicitly created to commemorate a person or event,serves as a memorial",
+  image:"assests/3.jpeg",
+  link:"variety.html"
+}
+]
+const bookvariety = 
+[
+  {
+  name: "Learning",
+  address:"Explicitly created to commemorate a person or event,serves as a memorial",
+  image:"assests/a.jpeg",
+  link:"variety.html"
+},
+  {
+  name: "Gaining Knowledge",
+  address:"Explicitly created to commemorate a person or event,serves as a memorial",
+  image:"assests/b.jpeg",
+  link:"variety.html"
+},
+  {
+  name: "Develop Yourself",
+  address:"Explicitly created to commemorate a person or event,serves as a memorial",
+  image:"assests/c.jpeg",
+  link:"variety.html"
+}
+]
