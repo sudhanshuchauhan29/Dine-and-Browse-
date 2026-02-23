e652110422328ee1c98453c4cacbd8a6a7477235
Sudhanshu Chauhan <chauhansudhanshu234@gmail.com>
2026-02-23 23:12:33 +0530

    Adding 2 js file in 1(app,app1 in app)

--- js/app.js
+++ js/app.js
@@ -21,3 +21,30 @@ function displayData(data, containerId) {
 
 displayData(restaurants, "restaurant-container");
 displayData(bookstores, "bookstore-container");
+
+
+
+function displayVariety(data, containerId) {
+  const container = document.getElementById(containerId);
+  container.innerHTML = "";
+
+  data.forEach(place => {
+    const card = `
+      <div class="boxe">
+        <div class="b"></div>
+        <div class="sboxe">
+          <a href="${place.link}" target="_blank">
+            <img src="${place.image}" alt="${place.name}">
+          </a>
+        </div>
+        <h4>${place.name}</h4>
+        <h3>${place.address}</h3>
+      </div>
+    `;
+    container.innerHTML += card;
+  });
+}
+
+
+displayVariety(restaurantvariety, "bookstore-variety");
+displayVariety(bookvariety, "restaurant-variety");
