594e81ebf5e1a9890f3704ebaf4aca5f10c8bc73
Sudhanshu Chauhan <chauhansudhanshu234@gmail.com>
2026-03-09 21:51:13 +0530

    Making Shukla Restaurant Dynamic

---
+++ js/restaurant.js
@@ -0,0 +1,27 @@
+const r = restaurantData;
+
+const container = document.getElementById("restaurant");
+
+container.innerHTML = `
+<h1>${r.name}</h1>
+
+<div class="container">
+${r.images.map(img => `<img src="${img}">`).join("")}
+</div>
+
+<h2>Address : ${r.address}</h2>
+<h2>Contact : ${r.contact}</h2>
+
+<p>${r.rating}</p>
+<p>Open : ${r.open}</p>
+
+<div class="map">
+<img src="${r.map}">
+</div>
+
+<div class="a">
+<a target="_blank" href="${r.link}">
+More Information
+</a>
+</div>
+`;
---
+++ js/shukla.js
@@ -0,0 +1,16 @@
+const restaurantData = {
+name: "SHUKLA SWEETS & FAMILY RESTAURANT",
+address: "SH 21, Tirwaganj, Kannauj",
+contact: "7947121510",
+rating: "4.1 (745 Ratings)",
+open: "6:00 AM to 11:00 PM",
+
+images: [
+"../assests/shukla1.webp",
+"../assests/shukla.webp"
+],
+
+map: "../assests/map_Shukla.jpg",
+
+link: "https://www.justdial.com/Kannauj/Shukla-Hotel-Restaurant"
+};
