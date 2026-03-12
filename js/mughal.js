5c686fa5f8edc34aa8dd65df5389b375bee30fe6
Sudhanshu Chauhan <chauhansudhanshu234@gmail.com>
2026-03-12 11:38:20 +0530

    Adding Mugal restaurant to my website

---
+++ js/mughal.js
@@ -0,0 +1,16 @@
+const restaurantData = {
+name: "MUGHAL ZAIKA RESTAURANTS",
+address: "Shukla Colony, Tirwaganj, Kannauj",
+contact: "7947434966",
+rating: "4.0 (439 Ratings)",
+open: "6:00 AM to 11:00 PM",
+
+images: [
+"../assests/mughal.jpg",
+"../assests/mughal2.jpg"
+],
+
+map: "../assests/mapmughal.jpg",
+
+link: "https://www.justdial.com/Kannauj/Mughal-Zaika-Near-Pathak-Pathology-Tirwa-Ganj/9999P5694-5694-220419013419-M5L4_BZDET?trkid=277-remotecity-fcat&term=restaurants&ncatid=10408936&area=Tirwa%20Ganj&search=Best%20Restaurants%20in%20Tirwa%20Ganj,%20Kannauj%20-%20Order%20Food%20Online&mncatname=Restaurants&abd_btn=&abd_heading=&bd=1&cat_b2b_flag=0&"
+};
--- js/shukla.js
+++ js/shukla.js
@@ -7,10 +7,10 @@ open: "6:00 AM to 11:00 PM",
 
 images: [
 "../assests/shukla1.webp",
-"../assests/shukla.webp"
+"../assests/shukla.jpg"
 ],
 
 map: "../assests/map_Shukla.jpg",
 
-link: "https://www.justdial.com/Kannauj/Shukla-Hotel-Restaurant"
+link: "https://www.justdial.com/Kannauj/Shukla-Hotel-Restaurant-Tirwa-Ganj/9999P5694-5694-180304083952-A1W2_BZDET"
 };
